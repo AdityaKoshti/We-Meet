f78d95793acfa5c370a9d914778229d60568481a
Pranav Todkar <75238140+pranavtodkar@users.noreply.github.com>
2022-06-27 13:04:48 +0530

    Initial react base

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Banner from './components/Banner';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,15 @@
+import bannerImg from '../images/landing page.png'
+const Banner = () => {
+  return (
+    <div>
+      <header className="header">
+        <h1>Just a test</h1>
+        <img src={bannerImg}></img>
+
+      </header>
+        
+    </div>
+  )
+}
+
+export default Banner
