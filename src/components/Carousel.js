ceabfb3a1dd965b32eaca3caede26f43ee8ed49e
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2022-06-29 14:47:54 +0530

    added multi carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,68 +1,58 @@
 import React from 'react'
+import card from '../assets/images/card.png'
+
 
 function Carousel() {
   return (
     <div>
-      <>
-        {/* Multi Carousel */}
-        <section id="Multi Carousel" data-aos="fade-down">
-          {/* TODO - change images links here */}
-          <div className="container-fluid text-center my-3">
-            <div className="row mx-auto my-auto justify-content-center">
-              <div
-                id="recipeCarousel"
-                className="carousel slide"
-                data-bs-ride="carousel"
-              >
-                <div className="carousel-inner" role="listbox">
-                  <div className="carousel-item active">
-                    <div className="col-md-3">
-                      <div className="card-img">
-                        <img
-                          src="src/assets/images/card.png"
-                          className="img-fluid custom-image"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="carousel-item">
-                    <div className="col-md-3">
-                      <div className="card-img">
-                        <img
-                          src="src/assets/images/card.png"
-                          className="img-fluid custom-image"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <a
-                  className="carousel-control-prev bg-transparent w-aut"
-                  href="#recipeCarousel"
-                  role="button"
-                  data-bs-slide="prev"
-                >
-                  <span
-                    className="carousel-control-prev-icon"
-                    aria-hidden="true"
-                  />
-                </a>
-                <a
-                  className="carousel-control-next bg-transparent w-aut"
-                  href="#recipeCarousel"
-                  role="button"
-                  data-bs-slide="next"
-                >
-                  <span
-                    className="carousel-control-next-icon"
-                    aria-hidden="true"
-                  />
-                </a>
-              </div>
+      <div
+        id="carouselExampleControls"
+        className="carousel carousel-dark slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner ">
+          <div className="carousel-item active">
+            <div className="card-wrapper d-flex justify-content-center">
+              <img src={card} className="d-block w-25 mx-4 px-4" alt="..." />
+              <img src={card} className="d-block w-25 mx-4 px-4" alt="..." />
+              <img src={card} className="d-block w-25 mx-4 px-4" alt="..." />
             </div>
           </div>
-        </section>
-      </>
+          <div className="carousel-item">
+          <div className="card-wrapper d-flex justify-content-center">
+              <img src={card} className="d-block w-25 mx-4 px-4" alt="..." />
+              <img src={card} className="d-block w-25 mx-4 px-4" alt="..." />
+              <img src={card} className="d-block w-25 mx-4 px-4" alt="..." />
+            </div>
+          </div>
+          <div className="carousel-item  ">
+          <div className="card-wrapper d-flex justify-content-center">
+              <img src={card} className="d-block w-25 mx-4 px-4" alt="..." />
+              <img src={card} className="d-block w-25 mx-4 px-4" alt="..." />
+              <img src={card} className="d-block w-25 mx-4 px-4" alt="..." />
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+
     </div>
   );
 }
