1b6aa744c45b4aef3ff2a2d365dead2b483f1bda
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2022-06-29 15:01:44 +0530

    added aos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-import React from 'react'
+import React,{useEffect} from 'react'
 import { BrowserRouter } from "react-router-dom";
 import MainRouter from "./MainRouter";
+import AOS from 'aos';
+import "aos/dist/aos.css";
+
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration : 500
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <MainRouter />
